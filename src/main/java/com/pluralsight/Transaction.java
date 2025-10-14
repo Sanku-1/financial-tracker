baee221a92195ee0ec7dbedcf339f86c20af3479
Stephen_A <ankustephen99@gmail.com>
2025-10-14 10:57:55 -0400

    Created Transaction.java class to house transaction data

--- src/main/java/com/pluralsight/Transaction.java
+++ src/main/java/com/pluralsight/Transaction.java
@@ -1,4 +1,18 @@
 package com.pluralsight;
 
 public class Transaction {
+    public String date;
+    public String time;
+    public String purchase;
+    public String vendorName;
+    public double purchasePrice;
+
+    public Transaction(String date, String time, String purchase, String vendorName, double purchasePrice){
+        this.date = date;
+        this.time = time;
+        this.purchase = purchase;
+        this.vendorName = vendorName;
+        this.purchasePrice = purchasePrice;
+    }
+
 }
