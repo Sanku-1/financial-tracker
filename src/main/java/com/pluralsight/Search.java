023b2e309e783050aafc03f72a080e9ad5b4f90d
Stephen_A <ankustephen99@gmail.com>
2025-10-16 19:57:09 -0400

    Fixed Search.java class constructor for streamlined use in Main

--- src/main/java/com/pluralsight/Search.java
+++ src/main/java/com/pluralsight/Search.java
@@ -11,109 +11,13 @@ public class Search {
     public double amount;
     public boolean after;
 
-    public Search(LocalDate startDate, LocalDate endDate, String description, String vendor, double amount) {
+    public Search(LocalDate startDate, LocalDate endDate, String description, String vendor, double amount, boolean after) {
         this.startDate = startDate;
         this.endDate = endDate;
-        this.description = description;
-        this.vendor = vendor;
-        this.amount = amount;
-        this.after = false;
-    }
-    public Search(LocalDate startDate, LocalDate endDate, String description, String vendor) {
-        this.startDate = startDate;
-        this.endDate = endDate;
-        this.description = description;
-        this.vendor = vendor;
-        this.after = false;
-        this.amount = 0;
-    }
-    public Search(LocalDate endDate, String description, String vendor, double amount) {
-        this.startDate = null;
-        this.endDate = endDate;
-        this.description = description;
-        this.vendor = vendor;
-        this.amount = amount;
-        this.after = false;
-    }
-    public Search(LocalDate startDate, String description, String vendor, double amount, boolean after) {
-        this.startDate = startDate;
-        this.endDate = null;
         this.description = description;
         this.vendor = vendor;
         this.amount = amount;
         this.after = after;
-    }
-    public Search(LocalDate startDate, LocalDate endDate, String vendor, double amount) {
-        this.startDate = startDate;
-        this.endDate = endDate;
-        this.description = "";
-        this.vendor = vendor;
-        this.amount = amount;
-        this.after = false;
-    }
-    public Search(LocalDate startDate, LocalDate endDate, double amount) {
-        this.startDate = startDate;
-        this.endDate = endDate;
-        this.description = "";
-        this.vendor = "";
-        this.amount = amount;
-        this.after = false;
-    }
-    public Search(LocalDate startDate, LocalDate endDate) {
-        this.startDate = startDate;
-        this.endDate = endDate;
-        this.description = "";
-        this.vendor = "";
-        this.amount = 0;
-        this.after = false;
-    }
-    public Search(LocalDate startDate, boolean after) {
-        this.startDate = startDate;
-        this.endDate = null;
-        this.description = "";
-        this.vendor = "";
-        this.amount = 0;
-        this.after = after;
-    }
-    public Search(LocalDate endDate) {
-        this.startDate = null;
-        this.endDate = endDate;
-        this.description = "";
-        this.vendor = "";
-        this.amount = 0;
-        this.after = false;
-    }
-    public Search(LocalDate endDate, String vendor) {
-        this.startDate = null;
-        this.endDate = endDate;
-        this.description = "";
-        this.vendor = vendor;
-        this.amount = 0;
-        this.after = false;
-    }
-    public Search(String description, LocalDate endDate) {
-        this.startDate = null;
-        this.endDate = endDate;
-        this.description = description;
-        this.vendor = "";
-        this.amount = 0;
-        this.after = false;
-    }
-    public Search(LocalDate endDate, double amount) {
-        this.startDate = null;
-        this.endDate = endDate;
-        this.description = "";
-        this.vendor = "";
-        this.amount = amount;
-        this.after = false;
-    }
-    public Search(String description, LocalDate endDate, double amount) {
-        this.startDate = null;
-        this.endDate = endDate;
-        this.description = description;
-        this.vendor = "";
-        this.amount = amount;
-        this.after = false;
     }
 
     public LocalDate getStartDate() {
